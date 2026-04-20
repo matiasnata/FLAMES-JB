5d9899f8a785613eb073228d10de3c9abfd21523
EliAntoniadis <elias200601@outlook.es>
2026-04-19 23:45:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask #importo libreria flask
 from routes.partidos import partidos_bp #traigo del partidos.py el blueprint
+from routes.Prode import prode_bp
 
 app = Flask(__name__) #creo la app, app seria el backend
 
 app.register_blueprint(partidos_bp) # agrego las rutas de partidos a la app
+app.register_blueprint(prode_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
